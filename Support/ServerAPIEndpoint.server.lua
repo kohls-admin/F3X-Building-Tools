ff95aa568b1772ef113cbc84de705715a14fc6e4
Kohl <kohltastrophe@gmail.com>
2026-05-10 16:08:10 -0500

    Refactor server endpoint setup

--- Support/ServerAPIEndpoint.server.lua
+++ Support/ServerAPIEndpoint.server.lua
@@ -1,11 +1,33 @@
-local ServerEndpoint = script.Parent;
-local SyncAPI = ServerEndpoint.Parent;
-local Tool = SyncAPI.Parent;
+local ServerEndpoint = script.Parent
+local SyncAPI = ServerEndpoint.Parent
+local Tool = SyncAPI.Parent
 
--- Start the server-side sync module
-SyncModule = require(SyncAPI:WaitForChild 'SyncModule');
+local Players = game:GetService("Players")
 
--- Provide functionality to the server API endpoint instance
-ServerEndpoint.OnServerInvoke = function (Client, ...)
-	return SyncModule.PerformAction(Client, ...);
-end;
+local function SETUP()
+	-- Start the server-side sync module
+	SyncModule = require(SyncAPI:WaitForChild "SyncModule")
+
+	-- Provide functionality to the server API endpoint instance
+	ServerEndpoint.OnServerInvoke = function(Client, ...)
+		return SyncModule.PerformAction(Client, ...)
+	end
+end
+
+local function isPlayerDescendant()
+	return Tool:IsDescendantOf(Players) or Players:GetPlayerFromCharacter(Tool.Parent)
+end
+
+ServerEndpoint.OnServerInvoke = function() end
+
+if isPlayerDescendant() then
+	SETUP()
+else
+	local conn
+	conn = Tool.AncestryChanged:Connect(function()
+		if isPlayerDescendant() then
+			conn:Disconnect()
+			SETUP()
+		end
+	end)
+end
